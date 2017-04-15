2ca02be93e96c28c086585e4f03f42abfa380e47
cdiaz1971 <cesar@cesardiaz.me>
2017-04-15 18:04:08 -0400

    script to move old dots and creat links

---
+++ makelinks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dotdir=~/dotfiles
+olddir=~/olddots
+files="bashrc vimrc bash_aliases tmux.conf"
+
+#create folder to back up dotsfiles
+echo "Creating olddots directory"
+mkidr -p $olddir
+echo "created"
+
+#creating dotfiles folder
+echo "Creating dotfiles folder"
+mkdir -p $dotdir
+echo "Created"
+
+cd $dotdir
+echo "moved into dotfiles directory"
+
+# move files and creat symlinks
+for file in $files; do
+	echo "Moving any existing dotfiles from ~ to $dotdir"
+	mv ~/.$file ~/olddots
+	echo "Creating symlink to $file in home directory."
+	ln -s $dotdir/$file ~/.$file
+done
